350a791464e384e4d88e772aaf9ef0a04b644f02
Ariel Churi <github@sparklelabs.com>
2016-10-19 14:15:45 -0400

    First commit

---
+++ myscript.js
@@ -0,0 +1,8 @@
+//alert("Hello World.");
+console.log("Hello World too.");
+var fredVariable;
+var tammyVariable;
+fredVariable = 42;
+tammyVariable = "HI!!!";
+console.log(fredVariable);
+console.log(fredVariable,tammyVariable);
